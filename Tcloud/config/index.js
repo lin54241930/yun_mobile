976bd4a75c9a3a132b758e578e33c7637ba155b2
ninesun <lin54241930@outlook.com>
2021-03-16 17:36:52 +0800

    修改Tcloud前端为所有IP都能访问

--- Tcloud/config/index.js
+++ Tcloud/config/index.js
@@ -13,15 +13,15 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    // host: '0.0.0.0', // can be overwritten by process.env.HOST
+    // host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
