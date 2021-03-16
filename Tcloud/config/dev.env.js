bb2613ffe42abc8c3925660edc0fe5c0160f3c0b
ninesun <lin54241930@outlook.com>
2021-03-16 17:47:15 +0800

    添加STF_URL，配置BASE_URL，COOKIE_DOMAIN
    这里的配置请根据自己的服务器IP来配置，以我本地服务器192.168.1.2为例

--- Tcloud/config/dev.env.js
+++ Tcloud/config/dev.env.js
@@ -5,17 +5,18 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // 接口地址配置
-  BASE_URL: '"http://localhost:9000"',
+  BASE_URL: '"http://192.168.1.2:9000"',
   //ws 服务的地址配置
   WS_BASE_URL:'"ws://xxxx"',
   //cookie 的过期时间
   COOKIE_EXPIRED: 14,
   //cookie 域名
-  COOKIE_DOMAIN: '"xxxxx"',
+  COOKIE_DOMAIN: '"192.168.1.2"',
   //cookie 存储前缀
   COOKIE_SUFFIX: '"_TCLOUD_DEV"',
   //企业微信扫码登录的相关配置
   QYWX_APPID: '"xxxx"',
   QYWX_AGENTID: '"xxxx"',
-  QYEX_REDIRECT_URI: '"xxxx"'
+  QYEX_REDIRECT_URI: '"xxxx"',
+  STF_URL:'"http://192.168.1.2:7100"'
 })
